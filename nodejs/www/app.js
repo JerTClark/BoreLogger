b665464f19a81596d9e512098656d40b98fc01a3
JerTClark <jercla317@gmail.com>
2015-04-07 01:58:32 -0400

    Rework nodejs
    Restores some original functionality of earlier website draft for landing
    and for web app. Eliminates some directories from tracking (.idea folders,
    build folders, etc.).

--- nodejs/www/app.js
+++ nodejs/www/app.js
@@ -6,34 +6,28 @@
 var http = require("http");
 var express = require("express");
 var errorHandler = require("errorhandler");
-var formidable = require("formidable");
-var fs_extra = require("fs-extra");
-var methodOverride = require("express-method-override");
-namespace = require("express-namespace");
-resource = require("express-resource");
+//var methodOverride = require("express-method-override");
+var namespace = require("express-namespace");
 
 //-- The Express app
 var app = new express();
 
 //-- My routes module (pass app as param)
-require("js/routes/routes.js")(app);
+require("./js/routes/routes.js")(app);
 
 //-- Stylus middleware
 //app.use(require("stylus").middleware("./public"));
 
 //-- Set a static resource on this filesystem that this app can use
-//app.use(express.static("backend"));
-//app.use(express.static("./assets"));
-//app.use(express.static("./lib"));
+app.use(express.static("./www/"));
 
 //-- Setting the view engine to Jade
 app.set("view engine", "jade");
-
 //-- Where to find the views
-app.set("views", "html");
+app.set("views", "www/html");
 
 //-- Method override
-app.use(methodOverride);
+//app.use(methodOverride);
 
 //-- TODO Remove in production
 if(app.get("env")==="development") {
@@ -43,6 +37,5 @@ if(app.get("env")==="development") {
 //-- The server
 var port = 3113;
 http.createServer(app).listen(port, function(){
-    console.log("Node.js-Express server starting on the localhost:" + port);
-    console.log("Bison, Inc. is now online and accepting connections.");
+    console.log("Accepting connections on localhost:" + port);
 });//-- Could also use code below
